98cf1e25dca85676be608d5e5a570600297e19bb
Alireza <alireza.kariminejad@yahoo.com>
2022-12-29 13:08:54 +0330

    Add widgets folder

---
+++ lib/widgets/background_shape_left.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LeftShape extends StatelessWidget {
+  const LeftShape({super.key, this.width = 0.0});
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          width: width,
+          height: 40.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            ),
+            color: Colors.green,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/background_shape_right.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RightShape extends StatelessWidget {
+  const RightShape({super.key, this.width = 0.0});
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          width: width,
+          height: 40.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              bottomLeft: Radius.circular(30.0),
+            ),
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+}
