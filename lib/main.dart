01dc2a4b7dba66f720cc4c41560180411c5896f3
Alireza <alireza.kariminejad@yahoo.com>
2022-12-29 13:10:01 +0330

    Send arguments to dart files in widgets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bmi_app/widgets/background_shape_left.dart';
+import 'package:flutter_bmi_app/widgets/background_shape_right.dart';
 
 void main() {
   runApp(const Application());
@@ -10,11 +12,25 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'dana'),
       home: Scaffold(
-          body: SafeArea(
-        child: Text('علیرضا متن نمایشی'),
-      )),
+        body: SafeArea(
+          child: Column(
+            children: [
+              RightShape(
+                width: 50.0,
+              ),
+              // SizedBox(height: 40.0),
+              LeftShape(width: 150.0),
+              // SizedBox(height: 40.0),
+              RightShape(width: 100.0),
+              // SizedBox(height: 40.0),
+              LeftShape(width: 50.0),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
