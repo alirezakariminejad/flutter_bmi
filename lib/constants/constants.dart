62eab9aced1403b7f446e9aa3fd257ef01a29744
Alireza <alireza.kariminejad@yahoo.com>
2023-01-06 12:44:34 +0330

    Add new color

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,3 +3,7 @@ import 'package:hexcolor/hexcolor.dart';
 
 final Color rubyColor = HexColor('#E91E63');
 final Color magentaColor = HexColor('#9C27B0');
+
+final Color greenMaterialColor = HexColor('#4CAF50');
+final Color redMaterialColor = HexColor('#F44336');
+final Color yellowMaterialColor = HexColor('#FFEB3B');
